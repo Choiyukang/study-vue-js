3348b1e855943e947befeb3017bae3d717c7507e
ykchoi <choe_gang@naver.com>
2022-08-10 10:12:52 +0900

    .

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- HTML -->
+  <div>header</div>
+</template>
+<script>
+
+export default {
+// JavaScript
+methods:{
+    addNum: function(){
+    }
+}
+}
+</script>
+
+<style>
+/* CSS */
+</style>
